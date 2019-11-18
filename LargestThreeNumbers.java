2cc7c8deac8bbb235c6b00192e094cf3b4e89b2e
Mladen Petrov <mladen.petrov88@gmail.com>
2019-11-18 18:46:11 +0200

    Add files via upload

---
+++ LargestThreeNumbers.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class LargestThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+
+        numbers.stream().sorted((a, b) -> b.compareTo(a)).limit(3).forEach(e -> System.out.print(e + " "));
+
+
+    }
+}
